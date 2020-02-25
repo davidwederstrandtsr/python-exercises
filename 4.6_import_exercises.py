2109002ef6b4a103cc8cde7da1e355a3fdc6560c
David Michael Wederstrandt Sr <david.wederstrandtsr@gmail.com>
2020-02-25 09:03:45 -0600

    updating 4.6_import with corrections

--- 4.6_import_exercises.py
+++ 4.6_import_exercises.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[ ]:
+# In[1]:
 
 
 # 1. Import and test 3 of the functions from your functions exercise file.
@@ -16,53 +16,53 @@
 #         use from and give the function a different name
 
 
-# In[1]:
+# In[2]:
 
 
 import functions_exercises
 
 
-# In[2]:
+# In[3]:
 
 
 assert functions_exercises.calculate_tip(10, 100) == 110
 assert functions_exercises.calculate_tip(10, 110) != 110
 
 
-# In[3]:
+# In[4]:
 
 
 from functions_exercises import handle_commas
 
 
-# In[4]:
+# In[5]:
 
 
 assert handle_commas('1,111') == 1111
 assert handle_commas('1,111') != 1110
 
 
-# In[5]:
+# In[6]:
 
 
 from functions_exercises import get_letter_grade as glg
 
 
-# In[6]:
+# In[7]:
 
 
 assert glg(86) == 'B'
 assert glg(86) != 'A'
 
 
-# In[ ]:
+# In[8]:
 
 
 # For the following exercises, read about and use the itertools module from the standard library 
 # to help you solve the problem.
 
 
-# In[ ]:
+# In[9]:
 
 
 # 1. How many different ways can you combine the letters from "abc" with the numbers 1, 2, and 3?
@@ -82,7 +82,7 @@ list(product("abc", ('123')))
 len(list(product("abc", ('123'))))
 
 
-# In[ ]:
+# In[12]:
 
 
 # 2. How many different ways can you combine two of the letters from "abcd"?
@@ -102,7 +102,7 @@ list(permutations("abcd", 2))
 len(list(permutations("abcd", 2)))
 
 
-# In[ ]:
+# In[15]:
 
 
 # Save this file as profiles.json inside of your exercises directory. Use the load function from the 
@@ -110,7 +110,7 @@ len(list(permutations("abcd", 2)))
 # code that calculates and outputs the following information:
 
 
-# In[15]:
+# In[16]:
 
 
 from json import load
@@ -122,145 +122,149 @@ from json import load
 
 
 
-# In[19]:
+# In[17]:
 
 
 profiles = load(open("profiles.json"))
 
 
-# In[ ]:
+# In[18]:
 
 
 # Total number of users
 
 
-# In[20]:
+# In[19]:
 
 
 len(profiles)
 
 
-# In[ ]:
+# In[20]:
 
 
 # Number of active users
 
 
-# In[25]:
+# In[21]:
 
 
 len([profile for profile in profiles if profile['isActive'] == True])
 
 
-# In[ ]:
+# In[22]:
 
 
 # Number of inactive users
 
 
-# In[27]:
+# In[23]:
 
 
 len([profile for profile in profiles if profile['isActive'] == False])
 
 
-# In[ ]:
+# In[24]:
 
 
 # Grand total of balances for all users
 
 
-# In[58]:
+# In[25]:
 
 
-def only_number(string):
-    new_string = string.replace(',','')
-    new_string = new_string.replace('$', '')
-    return float(new_string)
+def float_convertor(num):
+    return float(num.replace(',','').replace('$',''))
 
 
-# In[64]:
+# In[26]:
+
+
+float_convertor('$12,000')
+
+
+# In[27]:
 
 
 profile_balances = [profile['balance'] for profile in profiles]
 profile_balances
 
 
-# In[83]:
+# In[28]:
 
 
 newlist = []
 
 for profile in profile_balances:
-    newlist.append(only_number(profile))
+    newlist.append(float_convertor(profile))
 newlist
 
 
-# In[84]:
+# In[29]:
 
 
 total = sum(newlist)
 
 
-# In[85]:
+# In[30]:
 
 
 # Average balance per user
 
 
-# In[86]:
+# In[31]:
 
 
 total / len(newlist)
 
 
-# In[87]:
+# In[32]:
 
 
 # User with the lowest balance
 
 
-# In[88]:
+# In[33]:
 
 
 min(newlist)
 
 
-# In[96]:
+# In[34]:
 
 
 minlist = []
-minlist = [x for x in range(len(newlist)) if newlist[x] == min(new_list)]
+minlist = [x for x in range(len(newlist)) if newlist[x] == min(newlist)]
 minlist
 
 
-# In[103]:
+# In[35]:
 
 
 profiles[minlist[0]]['name'], profiles[minlist[0]]['balance']
 
 
-# In[ ]:
+# In[36]:
 
 
 # User with the highest balance
 
 
-# In[99]:
+# In[37]:
 
 
 maxlist = []
-maxlist = [x for x in range(len(newlist)) if newlist[x] == max(new_list)]
+maxlist = [x for x in range(len(newlist)) if newlist[x] == max(newlist)]
 maxlist
 
 
-# In[104]:
+# In[38]:
 
 
 profiles[maxlist[0]]['name'], profiles[maxlist[0]]['balance']
 
 
-# In[ ]:
+# In[39]:
 
 
 # Most common favorite fruit
@@ -272,64 +276,64 @@ profiles[maxlist[0]]['name'], profiles[maxlist[0]]['balance']
 
 
 
-# In[106]:
+# In[40]:
 
 
 fruitlist = [profile['favoriteFruit'] for profile in profiles]
 fruitlist
 
 
-# In[112]:
+# In[41]:
 
 
 fruit_count = [fruitlist.count(fruit) for fruit in fruitlist]
 fruit_count
 
 
-# In[118]:
+# In[42]:
 
 
 newfruitlist = list(zip(fruit_count, fruitlist))
 
 
-# In[122]:
+# In[43]:
 
 
 newfruitlist
 
 
-# In[119]:
+# In[44]:
 
 
 max(newfruitlist)
 
 
-# In[ ]:
+# In[45]:
 
 
 # Least most common favorite fruit
 
 
-# In[120]:
+# In[46]:
 
 
 min(newfruitlist)
 
 
-# In[ ]:
+# In[47]:
 
 
 # Total number of unread messages for all users
 
 
-# In[125]:
+# In[48]:
 
 
 greetinglist = [profile['greeting'] for profile in profiles]
 greetinglist
 
 
-# In[128]:
+# In[49]:
 
 
 def get_digits(string):
@@ -340,7 +344,7 @@ def get_digits(string):
     return new_string
 
 
-# In[132]:
+# In[50]:
 
 
 new = []
