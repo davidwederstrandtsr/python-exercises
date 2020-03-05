a53d4730672347a9d62a659e0d26f84a81092e46
David Wederstrandt Sr <david.wederstrandt@gmail.com>
2020-03-05 10:53:35 -0600

    updating

--- 4.8.4.3_dataframes.py
+++ 4.8.4.3_dataframes.py
@@ -1,28 +1,27 @@
 #!/usr/bin/env python
 # coding: utf-8
+# %%
 
-# In[1]:
-
-
+# %%
 import pandas as pd
 import numpy as np
 from pydataset import data
 
 
-# In[2]:
+# %%
 
 
 np.random.seed(123)
 
 
-# In[3]:
+# %%
 
 
 students = ['Sally', 'Jane', 'Suzie', 'Billy', 'Ada', 'John', 'Thomas',
             'Marie', 'Albert', 'Richard', 'Isaac', 'Alan']
 
 
-# In[4]:
+# %%
 
 
 # randomly generate scores for each student for each subject
@@ -34,7 +33,7 @@ reading_grades = np.random.randint(low=60, high=100, size=len(students))
 
 # #### create the dataframe
 
-# In[5]:
+# %%
 
 
 df = pd.DataFrame(
@@ -46,7 +45,7 @@ df = pd.DataFrame(
     })
 
 
-# In[6]:
+# %%
 
 
 df.describe()
@@ -54,7 +53,7 @@ df.describe()
 
 # ## 1. Copy the code from the lesson to create a dataframe full of student grades.
 
-# In[7]:
+# %%
 
 
 student_grades = df.copy()
@@ -63,7 +62,7 @@ student_grades
 
 # - Create a column named passing_english that indicates whether each student has a passing grade in reading.
 
-# In[8]:
+# %%
 
 
 student_grades['passing_english'] = student_grades.english > 70
@@ -73,7 +72,7 @@ student_grades
 
 # - Sort the english grades by the passing_english column. How are duplicates handled?
 
-# In[9]:
+# %%
 
 
 student_grades.sort_values(by="passing_english")
@@ -81,7 +80,7 @@ student_grades.sort_values(by="passing_english")
 
 # - Sort the english grades first by passing_english and then by student name. All the students that are failing english should be first, and within the students that are failing english they should be ordered alphabetically. The same should be true for the students passing english. (Hint: you can pass a list to the .sort_values method)
 
-# In[10]:
+# %%
 
 
 student_grades.sort_values(by=['passing_english', 'name'])
@@ -89,7 +88,7 @@ student_grades.sort_values(by=['passing_english', 'name'])
 
 # - Sort the english grades first by passing_english, and then by the actual english grade, similar to how we did in the last step.
 
-# In[11]:
+# %%
 
 
 student_grades.sort_values(by=['passing_english', 'english'])
@@ -97,7 +96,7 @@ student_grades.sort_values(by=['passing_english', 'english'])
 
 # - Calculate each students overall grade and add it as a column on the dataframe. The overall grade is the average of the math, english, and reading grades.
 
-# In[12]:
+# %%
 
 
 student_grades["overall_grade"] = (student_grades.math + student_grades.english + student_grades.reading) / 3
@@ -106,13 +105,13 @@ student_grades
 
 # ## 2. Load the mpg dataset. Read the documentation for the dataset and use it for the following questions:
 
-# In[13]:
+# %%
 
 
 data('mpg', show_doc=True) # view the documentation for the dataset
 
 
-# In[14]:
+# %%
 
 
 mpg = data('mpg')
@@ -121,7 +120,7 @@ mpg
 
 # - How many rows and columns are there?
 
-# In[15]:
+# %%
 
 
 mpg.shape
@@ -129,7 +128,7 @@ mpg.shape
 
 # - What are the data types of each column?
 
-# In[16]:
+# %%
 
 
 mpg.dtypes
@@ -137,13 +136,13 @@ mpg.dtypes
 
 # - Summarize the dataframe with .info and .describe
 
-# In[17]:
+# %%
 
 
 mpg.info()
 
 
-# In[18]:
+# %%
 
 
 mpg.describe()
@@ -151,7 +150,7 @@ mpg.describe()
 
 # - Rename the cty column to city.
 
-# In[19]:
+# %%
 
 
 mpg = mpg.rename(columns={'cty': 'city'})
@@ -160,7 +159,7 @@ mpg
 
 # - Rename the hwy column to highway.
 
-# In[20]:
+# %%
 
 
 mpg = mpg.rename(columns={'hwy':'highway'})
@@ -169,7 +168,7 @@ mpg
 
 # - Do any cars have better city mileage than highway mileage?
 
-# In[21]:
+# %%
 
 
 mpg[mpg.city > mpg.highway]
@@ -177,7 +176,7 @@ mpg[mpg.city > mpg.highway]
 
 # - Create a column named mileage_difference this column should contain the difference between highway and city mileage for each car.
 
-# In[22]:
+# %%
 
 
 mpg["milage_difference"] = mpg.highway - mpg.city
@@ -186,7 +185,7 @@ mpg
 
 # - Which car (or cars) has the highest mileage difference?
 
-# In[23]:
+# %%
 
 
 mpg.nlargest(n=1, columns='milage_difference', keep='all')
@@ -194,13 +193,13 @@ mpg.nlargest(n=1, columns='milage_difference', keep='all')
 
 # - Which compact class car has the lowest highway mileage? The best?
 
-# In[24]:
+# %%
 
 
 mpg[mpg['class'] == ('compact')].nsmallest(n=1, columns="highway", keep="all")
 
 
-# In[25]:
+# %%
 
 
 mpg.nlargest(n=1, columns="highway", keep="all")
@@ -208,7 +207,7 @@ mpg.nlargest(n=1, columns="highway", keep="all")
 
 # - Create a column named average_mileage that is the mean of the city and highway mileage.
 
-# In[26]:
+# %%
 
 
 mpg['average_milage'] = (mpg.city + mpg.highway) / 2
@@ -217,13 +216,13 @@ mpg
 
 # - Which dodge car has the best average mileage? The worst?
 
-# In[27]:
+# %%
 
 
 mpg[mpg['manufacturer'] == ('dodge')].nlargest(n=1, columns="average_milage", keep="all")
 
 
-# In[28]:
+# %%
 
 
 mpg[mpg['manufacturer'] == ('dodge')].nsmallest(n=1, columns="average_milage", keep="all")
@@ -231,7 +230,7 @@ mpg[mpg['manufacturer'] == ('dodge')].nsmallest(n=1, columns="average_milage", k
 
 # ## 3. Load the Mammals dataset. Read the documentation for it, and use the data to answer these questions:
 
-# In[29]:
+# %%
 
 
 Mammals = data('Mammals')
@@ -239,7 +238,7 @@ Mammals = data('Mammals')
 
 # - How many rows and columns are there?
 
-# In[30]:
+# %%
 
 
 Mammals.shape
@@ -247,7 +246,7 @@ Mammals.shape
 
 # - What are the data types?
 
-# In[31]:
+# %%
 
 
 Mammals.dtypes
@@ -255,13 +254,13 @@ Mammals.dtypes
 
 # - Summarize the dataframe with .info and .describe
 
-# In[32]:
+# %%
 
 
 Mammals.info()
 
 
-# In[33]:
+# %%
 
 
 Mammals.describe()
@@ -269,7 +268,7 @@ Mammals.describe()
 
 # - What is the the weight of the fastest animal?
 
-# In[34]:
+# %%
 
 
 Mammals.sort_values(by='speed', ascending=False).head(1).weight
@@ -277,7 +276,7 @@ Mammals.sort_values(by='speed', ascending=False).head(1).weight
 
 # - What is the overal percentage of specials?
 
-# In[35]:
+# %%
 
 
 Mammals['specials'].sum() / len(Mammals.index) * 100
@@ -285,21 +284,21 @@ Mammals['specials'].sum() / len(Mammals.index) * 100
 
 # - How many animals are hoppers that are above the median speed? What percentage is this?
 
-# In[36]:
+# %%
 
 
 Mammals.head(4)
 median_speed = Mammals['speed'].median()
 
 
-# In[37]:
+# %%
 
 
 fastest = Mammals[(Mammals['hoppers'] == (True)) & (Mammals['speed'] > median_speed)]
 len(fastest)
 
 
-# In[38]:
+# %%
 
 
 len(fastest) / len(Mammals) * 100
---
+++ matplotlib_exercises.py
@@ -0,0 +1,171 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# **Use matplotlib to plot the following equation using LaTex:**
+# 
+# You'll need to write the code that generates the x and y points.
+# 
+# Add an anotation for the point **0, 0** , the origin.
+
+get_ipython().run_line_magic('matplotlib', 'inline')
+
+import matplotlib.pyplot as plt
+import math
+
+# Problem 1 - 
+# Use matplotlib to plot the following equation:
+
+x = range(10)
+y = [x**2 - x + 2 for x in x]
+
+plt.plot(x, y)
+plt.annotate('origin', xy=(0, 2), xytext=(0,  10),
+            arrowprops={'facecolor': 'cyan'})
+plt.xlabel('x')
+plt.ylabel('y')
+plt.title('$y = x^2 - x + 2$')
+plt.show()
+
+
+# Problem 2 - 
+# Create and label 4 separate charts for the following equations 
+# (choose a range for x that makes sense):
+
+x = range(10)
+y = [math.sqrt(x) for x in x]
+
+plt.plot(x, y)
+plt.annotate('origin', xy=(0, .2), xytext=(0,  1),
+            arrowprops={'facecolor': 'cyan'})
+plt.xlabel('x')
+plt.ylabel('y')
+plt.title('$y = \\sqrt{x}$')
+plt.show()
+
+x = range(6)
+y = [x**3 for x in x]
+
+plt.plot(x, y)
+plt.annotate('origin', xy=(0, 5), xytext=(0,  20),
+            arrowprops={'facecolor': 'cyan'})
+plt.xlabel('x')
+plt.ylabel('y')
+plt.title('$y = x^3$')
+plt.show()
+
+x = range(1000)
+y = [math.tan(x) for x in x]
+
+plt.plot(x, y)
+plt.title('$y = \\tan(x)$')
+plt.show()
+
+x = range(5)
+y = [2**x for x in x]
+
+plt.plot(x, y)
+plt.annotate('origin', xy=(0, 1), xytext=(0,  4),
+            arrowprops={'facecolor': 'cyan'})
+plt.xlabel('x')
+plt.ylabel('y')
+plt.title('$y = 2^x$')
+plt.show()
+
+
+# ### Problem 3 
+# Combine the figures you created in the last step into one large figure with 4 subplots.
+
+# y = x^2 - x + 2
+x1 = range(15)
+y1 = [x1**2 - x1 + 2 for x1 in x1]
+
+# y = sqrt(x)
+x2 = range(200)
+y2 = [math.sqrt(x2) for x2 in x2]
+
+# y = x^3
+x3 = range(8)
+y3 = [x3**3 for x3 in x3]
+
+# y = tan(x)
+x4 = range(1000)
+y4 = [math.tan(x4) for x4 in x4]
+
+# y = 2^x
+x5 = range(10)
+y5 = [2**x5 for x5 in x5]
+
+plt.figure(figsize=(18, 18))
+plt.suptitle('5 plots condensed to one')
+
+# Figure 1
+plt.subplot(3, 2, 1)
+plt.plot(x1, y1)
+plt.xlabel('x')
+plt.ylabel('y')
+plt.title('$y = x^2 - x + 2$')
+
+# Figure 2
+plt.subplot(3, 2, 2)
+plt.plot(x2, y2)
+plt.xlabel('x')
+plt.ylabel('y')
+plt.title('$y = \\sqrt{x}$')
+
+# Figure 3
+plt.subplot(3, 2, 3)
+plt.plot(x3, y3)
+plt.xlabel('x')
+plt.ylabel('y')
+plt.title('$y = x^3$')
+
+# Figure 4
+plt. subplot(3, 2, 4)
+plt.plot(x4, y4)
+plt.title('$y = \\tan(x)$')
+
+# Figure 5
+plt.subplot(3, 2, 5)
+plt.plot(x5, y5)
+plt.xlabel('x')
+plt.ylabel('y')
+plt.title('$y = 2^x$')
+plt.show()
+
+
+# ### Problem 4
+# 
+# Combine the figures you created in the last step into one figure where each of the 4 equations
+# has a different color for the points. Be sure to include a legend.
+
+plt.figure(figsize=(16, 8))
+plt.suptitle('5 plots condensed to one')
+
+# Figure 1
+plt.plot(x1, y1, c='red', label='$y = x^2 - x + 2$')
+plt.xlabel('x')
+plt.ylabel('y')
+
+# Figure 2
+plt.plot(x2, y2, c='green', label='$y = \\sqrt{x}$')
+plt.xlabel('x')
+plt.ylabel('y')
+
+# Figure 3
+plt.plot(x3, y3, c='orange', label='$y = x^3$')
+plt.xlabel('x')
+plt.ylabel('y')
+
+# Figure 4
+plt.plot(x4, y4, c='pink', ls='--', label='$y = \\tan(x)$')
+
+# Figure 5
+plt.plot(x5, y5, label='$y = 2^x$')
+plt.xlabel('x')
+plt.ylabel('y')
+
+# picture size and info 
+plt.ylim(-250, 350)
+plt.xlim(0, 16)
+plt.legend()
+plt.show()
